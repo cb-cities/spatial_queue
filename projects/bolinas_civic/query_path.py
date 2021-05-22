6136e72731f06f12afd4eb650e1912cd3d1f5fe9
bz247 <bingyu.zhao@yahoo.com>
2021-05-21 22:14:02 -0700

    output the length of the next link

--- projects/bolinas_civic/query_path.py
+++ projects/bolinas_civic/query_path.py
@@ -35,10 +35,11 @@ def query_path(vphh=None, visitor_cnts=None, player_origin=None, player_destin=N
     node2link_dict = json.load(open(read_path + '/simulation_outputs/network/node2link_dict.json'))
 
     link_speed_dict = json.load(open(read_path + '/simulation_outputs/link_weights/link_speed_vphh{}_visitor{}.json'.format(vphh, visitor_cnts)))
+    link_length_dict = {getattr(l, 'eid'): getattr(l, 'length') for l in links_df.itertuples()}
     
     current_link = 'n{}_vl'.format(player_origin)
     current_link_distance = 0
-    current_link_angle = 0
+    # current_link_angle = 0
 
     # all nodes that player passed
     player_nodes = [player_origin]
@@ -67,12 +68,12 @@ def query_path(vphh=None, visitor_cnts=None, player_origin=None, player_destin=N
             next_node = player_route_by_nodes[network_links[str(current_link)]['end_nid']]
             current_link = node2link_dict[str(network_links[str(current_link)]['end_nid'])][str(next_node)]
             player_nodes.append(network_links[str(current_link)]['end_nid'])
-            player_nodes_time_traffic.append((network_links[str(current_link)]['start_nid'], t_p, link_speed_dict[str(t_p)][current_link]))
+            player_nodes_time_traffic.append((network_links[str(current_link)]['start_nid'], t_p, link_speed_dict[str(t_p)][current_link], link_length_dict[current_link]))
             # print('new link {} at {}\n'.format(current_link, t_p))  
     
     print('vehicle is on link {} at {} seconds. The end node ID of the current link is {}'.format(current_link, end_time, next_node))
     print('Player path nodes {}'.format(player_nodes))
-    print('Player path nodes/time/speed_of_next_link {}'.format(player_nodes_time_traffic))
+    print('Player path nodes/time/speed_of_next_link/length_of_next_link {}'.format(player_nodes_time_traffic))
     # traffic: light: > 9 m/s, medium: 3-9 m/s, heavy: < 3 m/s
 
     return player_nodes, player_nodes_time_traffic
